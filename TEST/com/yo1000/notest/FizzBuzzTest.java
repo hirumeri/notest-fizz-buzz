beed109f46fdfd792baecb4da3eec18edb8c445e
hirumeri <takurotanaka@outlook.jp>
2020-11-26 20:27:19 +0900

    test init

---
+++ TEST/com/yo1000/notest/FizzBuzzTest.java
@@ -0,0 +1,46 @@
+package com.yo1000.notest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void run() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fizzBuzz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fizz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dizz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bizz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void buzz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void duzz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fuzz() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void result() {
+    }
+}
